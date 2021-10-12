4d2b0a9e408fa6681754ed2a3a4b1567c9ca6558
Acuario11 <madaichuco@upeu.edu.pe>
2021-10-11 21:32:51 -0500

    tc

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,4 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+//import 'package:http/http.dart' as http;
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 
@@ -14,15 +18,32 @@ class _FullScreenMapState extends State<FullScreenMap> {
   final center = LatLng(-6.472352016305042, -76.39615172024821);
   //final center = LatLng(-6.0753924080396216, -76.96540920171033);
   String selectedStyle = 'mapbox://styles/madaichuco/ckun34e991c2s18lah30rlqv0';
-
+  final satellite = 'mapbox://styles/madaichuco/ckunbxqf19twj18p98zo5kflc';
   final streets_g = 'mapbox://styles/madaichuco/ckun3vc1h6i1519mwqgkew02m';
   final mono_verde= 'mapbox://styles/madaichuco/ckun34e991c2s18lah30rlqv0';
 
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
-
+    //_onStyleLoaded();
   }
 
+  /*void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/custom-icon.png");
+    addImageFromUrl("networkImage", "https://via.placeholder.com/50");
+  }
+
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController!.addImage(name, list);
+  }
+
+  /// Adds a network image to the currently displayed style
+  Future<void> addImageFromUrl(String name, url) async {
+    var response = await http.get(url);
+    return mapController!.addImage(name, response.bodyBytes);
+  }
+ */
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -42,7 +63,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
            onPressed: (){
              mapController?.addSymbol( SymbolOptions(
              geometry: center,
-             iconSize: 2.5,
+             //iconSize: 2.5,
              iconImage: 'attraction-15',
              textField: 'Montaña creada aquí',
              //textColor: '#cccccc' ,
@@ -59,11 +80,6 @@ class _FullScreenMapState extends State<FullScreenMap> {
 
            //mapController?.animateCamera(CameraUpdate.tiltTo(40));
            mapController?.animateCamera(CameraUpdate.zoomIn());
-           /*mapController?.addSymbol( SymbolOptions(
-             geometry: center,
-             textField: 'Montaña creada aquí',
-             textColor: '#cccccc' ,
-           ));*/
          }
        ),
        SizedBox(height: 5),
@@ -89,6 +105,8 @@ class _FullScreenMapState extends State<FullScreenMap> {
             selectedStyle = streets_g;
           }else{
             selectedStyle = mono_verde;
+          }if(selectedStyle == streets_g){
+            selectedStyle = satellite;
           }
 
           setState(() {});
